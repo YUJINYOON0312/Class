f051a84ee91d50fe6746600f3acd2e97ba7e930e
YuJin Yoon <107790324+YUJINYOON0312@users.noreply.github.com>
2022-09-01 12:44:31 +0900

    Add files via upload
    20220824

---
+++ LoopTest.java
@@ -0,0 +1,21 @@
+package day0824;
+
+public class LoopTest {
+
+	public static void main(String[] args) {
+	// 구구단 츨력
+	
+		
+		for(int num = 2; num <= 9; num++) {
+		for(int i = 1; i<=9; i++) {
+		
+			System.out.println(num +" * "+ i + " = " + i * num);
+		
+			}
+		
+			System.out.println("");
+		}
+
+	}
+
+} // 구구단 2-9단 출력

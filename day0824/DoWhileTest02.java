db0a5807091b72ad39d36d6eca4fd8c8c3454a2e
YuJin Yoon <107790324+YUJINYOON0312@users.noreply.github.com>
2022-09-01 12:55:02 +0900

    Add files via upload

---
+++ day0824/DoWhileTest02.java
@@ -0,0 +1,26 @@
+package day0824;
+
+public class DoWhileTest02 {
+
+	public static void main(String[] args) {
+	
+		
+		int out = 0;
+			do{
+				int i = 0;
+				do {
+					System.out.print("* ");
+					i++;
+				}while(i<5);
+	
+				System.out.println();
+				out++;
+			}while(out<5);
+	}
+
+}
+//* * * * * 
+//* * * * * 
+//* * * * * 
+//* * * * * 
+//* * * * * 
